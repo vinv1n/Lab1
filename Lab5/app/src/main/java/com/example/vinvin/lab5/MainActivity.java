95180df3d372e890497bc3d415e28fdafc474406
Juho Sipola <jusipola@student.oulu.fi>
2018-02-28 01:16:55 +0200

    Lab5 finished

--- Lab5/app/src/main/java/com/example/vinvin/lab5/MainActivity.java
+++ Lab5/app/src/main/java/com/example/vinvin/lab5/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.vinvin.lab5;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
+
+    private static final int MY_PERMISSIONS = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        checkPermissions();
+
         setupControls();
+
+        launchTwitterStream();
     }
 
     @Override
@@ -62,29 +74,66 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void lauchTwitterStream(){
+    private void launchTwitterStream(){
+
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         final Intent twitterAuth = new Intent(this, TwitterActivity.class);
 
         if (preferences.contains(TwitterActivity.TWITTER_OAUTH_KEY) &&
-                preferences.contains(TwitterActivity.TWITTER_OAUTH_SECRET)){
+                preferences.contains(TwitterActivity.TWITTER_OAUTH_SECRET)) {
+        }
 
-            Intent tweet = new Intent(this, TwitterActivity.class);
-            startActivity(tweet);
+        Intent tweet = new Intent(this, TwitterActivity.class);
+        startActivity(tweet);
 
-        } else {
+         /*else {
             // nothing in the layout
             setContentView(R.layout.login_layout);
             // looks horrible :DDD
-            ImageView login = (ImageView) findViewById(R.id.login_button);
+            Button login = (Button) findViewById(R.id.login_button);
             login.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     twitterAuth.setAction(TwitterActivity.ACTION_TWITTER_AUTHENTICATE);
-                    startService(twitterAuth);
+                    startActivity(twitterAuth);
                 }
             });
+        }*/
+    }
+
+    private void checkPermissions(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_NETWORK_STATE)
+                != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.INTERNET)){
+                // nothing
+                //
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.
+                        INTERNET, Manifest.permission.ACCESS_NETWORK_STATE}, MY_PERMISSIONS);
+
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MY_PERMISSIONS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                    // twitter api
+                    launchTwitterStream();
+                } else {
+                    // no permissions
+                    finish();
+                }
         }
     }
 }
--- Lab5/app/src/main/java/com/example/vinvin/lab5/TwitterActivity.java
+++ Lab5/app/src/main/java/com/example/vinvin/lab5/TwitterActivity.java
@@ -13,23 +13,26 @@ import android.preference.PreferenceManager;
 import android.provider.Browser;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import twitter4j.*;
 import twitter4j.auth.AccessToken;
-import twitter4j.auth.OAuth2Token;
 import twitter4j.auth.RequestToken;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
- *
  * Created by vinvin on 26.2.2018.
  */
 
@@ -45,6 +48,14 @@ public class TwitterActivity extends Activity {
 
     static RequestToken authToken;
 
+    ImageView imageView;
+    TextView textView;
+    ListView listView;
+    List<Status> tweets;
+    ArrayAdapter<Status> adapter;
+
+    static final StringBuilder builder = new StringBuilder();
+
     static final String ACTION_TWITTER_AUTHENTICATE = "ACTION_TWITTER_AUTHENTICATE";
     static final String ACTION_TWITTER_ACCESS = "ACTION_TWITTER_ACCESS";
     String EXTRA_OAUTH_DATA = "EXTRA_OAUTH_DATA";
@@ -56,53 +67,59 @@ public class TwitterActivity extends Activity {
         view = (FrameLayout) findViewById(R.id.list_item);
 
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        twitter = getTwitter();
 
-        if (getIntent().getAction().equalsIgnoreCase(ACTION_TWITTER_AUTHENTICATE)) {
-            authenticate();
-        }
-        if (getIntent().getAction().equalsIgnoreCase(ACTION_TWITTER_ACCESS)){
+        //imageView = (ImageView) findViewById(R.id.tweet_picture);
+        listView = (ListView) findViewById(R.id.main_list);
+
+        /*if (getIntent().getAction().equalsIgnoreCase(ACTION_TWITTER_AUTHENTICATE)) {
             getTwitterAccess();
         }
+        if (getIntent().getAction().equalsIgnoreCase(ACTION_TWITTER_ACCESS)) {
+            getTwitterAccess();
+        }*/
+        getTweets();
 
     }
 
-    private boolean isAuthenticated(){
+    private boolean isAuthenticated() {
         return preferences.getBoolean(PREF_KEY_TWITTER_LOGIN, false);
     }
 
-    private Twitter getTwitter(){
+    private Twitter getTwitter() {
 
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
         configurationBuilder.setOAuthConsumerKey(getString(R.string.app_key_consumer))
                 .setOAuthConsumerSecret(getString(R.string.app_key_secret))
-                .setOAuthAccessToken(preferences.
-                        getString(getString(R.string.access_token), ""))
-                .setOAuthAccessTokenSecret(preferences.
-                        getString(getString(R.string.access_token_secret), ""));
+                .setOAuthAccessToken(getString(R.string.access_token))
+                .setOAuthAccessTokenSecret(getString(R.string.access_token_secret));
 
         TwitterFactory factory = new TwitterFactory(configurationBuilder.build());
         twitter = factory.getInstance();
         return twitter;
     }
 
-    private void getTweets(){
+    private void getTweets() {
+
         new Thread(new Runnable() {
 
             @Override
             public void run() {
                 try {
-                    List<Status> tweets = getTwitter().getHomeTimeline();
+                    tweets = getTwitter().getHomeTimeline();
+                    final List<String> list_tweets = new ArrayList<>();
                     for (final Status status : tweets) {
-                        View tweetsLayout = LayoutInflater.from(getApplicationContext()).inflate(R.
-                                layout.tweet_row, null);
-                        ImageView imageView = (ImageView) findViewById(R.id.tweet_picture);
-                        TextView textView = (TextView) findViewById(R.id.tweet_text);
-                        Bitmap myImage = BitmapFactory.decodeFile(status.getUser().
-                                getBiggerProfileImageURL());
-                        imageView.setImageBitmap(myImage);
-                        textView.setText(status.getText());
-                        view.addView(tweetsLayout);
+                        list_tweets.add(status.getText());
                     }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // not pretty solution
+                            ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(),
+                                    R.layout.list_item, list_tweets);
+                            listView.setAdapter(adapter);
+                        }
+                    });
                 } catch (twitter4j.TwitterException e) {
                     // no fancy error handling
                     Toast.makeText(getApplicationContext(), "Something went wrong",
@@ -112,8 +129,7 @@ public class TwitterActivity extends Activity {
         }).start();
     }
 
-
-    public void authenticate(){
+    public void authenticate() {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -124,26 +140,29 @@ public class TwitterActivity extends Activity {
                     auth.setData(Uri.parse(authToken.getAuthenticationURL()));
                     auth.putExtra(Browser.EXTRA_APPLICATION_ID, getPackageName());
                     startActivity(auth);
-                } catch (twitter4j.TwitterException e){
+                    finish();
+                } catch (twitter4j.TwitterException e) {
                     finish();
                 }
             }
         }).start();
+        getTwitterAccess();
     }
 
-    private void getTwitterAccess(){
+    private void getTwitterAccess() {
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    AccessToken accessToken = twitter.getOAuthAccessToken(authToken,
+                    AccessToken accessToken = twitter.getOAuthAccessToken(twitter.getOAuthRequestToken(),
                             getIntent().getStringExtra(EXTRA_OAUTH_DATA));
                     preferences.edit()
                             .putString(TWITTER_OAUTH_KEY, accessToken.getToken())
                             .putString(TWITTER_OAUTH_SECRET, accessToken.getTokenSecret())
                             .apply();
-                } catch (twitter4j.TwitterException e){
+                    finish();
+                } catch (twitter4j.TwitterException e) {
                     finish();
                 }
             }
